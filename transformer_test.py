4b502693d1217c3e733f6aecdc8fef546d4d18bc
LXQBLESS <k530003036@mail.uic.edu.hk>
2019-09-24 20:50:06 +0800

    Initial commit

---
+++ transformer_test.py
@@ -0,0 +1,12 @@
+import os
+import time
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import logging
+from pprint import pprint
+from IPython.display import clear_output
+
+print(tf.__version__)
